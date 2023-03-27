3136d9de7709a5b62349a27b1bc742caef6f8a8a
Jacob Bennett <jacobwbennett@gmail.com>
2023-03-27 00:09:43 -0600

    Add revalidate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { SidemenuProvider } from '../context/useSidemenu';
 import SideMenu from '../components/app/sideMenu';
 import Hamburger from '../components/app/hamburger';
 
+export const revalidate = 60;
+
 const name = process.env.NAME as string;
 
 export default function RootLayout({
